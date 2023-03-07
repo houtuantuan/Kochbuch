901bab3bd99285b1a51ad2a7d3e82722fdbc3235
houtuantuan <80598603+houtuantuan@users.noreply.github.com>
2023-03-07 17:51:48 +0100

    new components

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import { createClient } from "contentful";
+import { useEffect, useState } from "react";
+import {Route,Routes} from "react-router-dom";
+
+
+function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  const client = createClient({
+    space: "g7wwoqw7k7vb",
+    accessToken: "6vacSQq_qoG6HEtSss7ZTCoHm74Sr8yTUUEbOK6PXDY",
+  });
+
+  const getData = async () => {
+    const entryItems = await client.getEntries();
+    console.log(1111)
+    console.log("ENTRIES: ", entryItems.items);
+    setRecipes(entryItems.items);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+  return (
+    <div className="App">
+      <h1>CONTENTFUL TEST</h1>
+      {recipes.map((recipe, index) => (
+        <div key={index}>
+          <p>{recipe.fields.rezeptName}</p>
+          <img src={recipe.fields.rezeptBilder.fields.file.url} />
+          <ul>
+            {recipe.fields.anleitung.map((e)=>(<p>{e}</p>))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
