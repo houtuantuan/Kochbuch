e980c2d2882cb169c9ca4aaf5b3923d51a21d29e
houtuantuan <80598603+houtuantuan@users.noreply.github.com>
2023-03-08 09:42:45 +0100

    use bootstrap and react to show the pics

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-export default function Main() {
-  return (
-    <div>Main</div>
-  )
+export default function Main({ recipe }) {
+    return (
+
+        <Card bg='light' border="success" className='picContainer'>
+            <Card.Img 
+            className="recipePic" variant="top" src={recipe.fields.rezeptBilder.fields.file.url} />
+            <Card.Body>
+                <Card.Title>{recipe.fields.rezeptName}</Card.Title>
+                <Card.Text>
+                    This is a longer card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit
+                    longer.
+                </Card.Text>
+                <Button variant="primary">check it</Button>
+            </Card.Body>
+        </Card>
+    )
 }
