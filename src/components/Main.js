e952a129691badd060d17bd9db0316e9e5c60ee5
houtuantuan <80598603+houtuantuan@users.noreply.github.com>
2023-03-09 18:18:44 +0100

    kochzeit hinzugefügt

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,9 +11,7 @@ export default function Main({ recipe }) {
             <Card.Body>
                 <Card.Title>{recipe.fields.rezeptName}</Card.Title>
                 <Card.Text>
-                    This is a longer card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
+                    {recipe.fields.beschreibung}
                 </Card.Text>
                 <Button variant="primary">check it</Button>
             </Card.Body>
